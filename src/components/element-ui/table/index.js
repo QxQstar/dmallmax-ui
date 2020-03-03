3455bf90dbff6d01f60f61d71be554a8dd428664
秦小倩 <qinxiaoqian@lsh123.com>
2020-03-03 10:43:51 +0800

    feat: 重新实现table 组件

---
+++ src/components/element-ui/table/index.js
@@ -0,0 +1,9 @@
+/*eslint-disable*/
+import ElTable from './src/table';
+
+/* istanbul ignore next */
+ElTable.install = function(Vue) {
+  Vue.component(ElTable.name, ElTable);
+};
+
+export default ElTable;
